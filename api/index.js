35d3d826fd9653de189f90ebc0551645830cb560
= <=>
2021-08-28 13:56:03 +1000

    Update CORS in API

--- api/index.js
+++ api/index.js
@@ -15,7 +15,9 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}))
 app.use(helmet())
 
 // Setting the below creates the root to be used with all of the remaining routes
