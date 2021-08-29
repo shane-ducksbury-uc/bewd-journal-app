4f065ae01fb0284765aa301f1890c3888916362e
= <=>
2021-08-29 18:34:39 +1000

    Update

--- api/config.js
+++ api/config.js
@@ -9,5 +9,5 @@ export const createDbConnection = () => {
     host: `${process.env.POSTGRES_HOST}`,
     database: `${process.env.POSTGRES_DATABASE}`,
     password: `${process.env.POSTGRES_PASSWORD}`,
-    port: `${process.env.POSTGRES_PORT}`,
+    port: 5432,
 })}
